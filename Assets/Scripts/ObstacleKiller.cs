977bf50da12055a7cbf7ab1597e9c2feb4e08ffb
Ernest-2020 <73068574+Ernest-2020@users.noreply.github.com>
2021-10-09 12:21:49 +0300

    added lesson2

---
+++ Assets/Scripts/ObstacleKiller.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class ObstacleKiller : MonoBehaviour
+    {
+        [SerializeField] 
+        private Transform _player;
+        [SerializeField]
+        private ObstacleSpawner _spawner;
+        [SerializeField]
+        private float _killDistanceZ;
+
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            List<Transform> obstacles = _spawner.spawnedObstacles;
+
+            for (int i = 0; i <obstacles.Count; i++)
+            {
+                if (_player.position.z > obstacles[i].position.z +_killDistanceZ)
+                {
+                    Destroy(obstacles[i].gameObject);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/StartButton.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+namespace Assets.Scripts
+{
+    public class StartButton : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject _buttonStart;
+        public void ButtonStart()
+        {
+            Time.timeScale = 1;
+            _buttonStart.SetActive(false);
+        }
+    }
+}
